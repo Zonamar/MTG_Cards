c1472e8cab56c84f56c0b0d38746090d52d4d9cc
Zonamar <ZonamarMac@Christians-iMac.home>
2015-03-30 14:25:50 -0500

    basic Magic Card class private vars setup

---
+++ MagicRule.cpp
@@ -0,0 +1,9 @@
+//
+//  MagicRule.cpp
+//  MTG_Practice
+//
+//  Created by Christian Larsen on 3/30/15.
+//
+//
+
+#include "MagicRule.h"
---
+++ MagicRule.h
@@ -0,0 +1,31 @@
+//
+//  MagicRule.h
+//  MTG_Practice
+//
+//  Created by Christian Larsen on 3/30/15.
+//
+//
+
+#ifndef __MTG_Practice__MagicRule__
+#define __MTG_Practice__MagicRule__
+
+#include <stdio.h>
+
+class MagicRule
+{
+    
+public:
+    
+    
+    
+    
+private:
+    
+    
+    
+    
+    
+};
+
+
+#endif /* defined(__MTG_Practice__MagicRule__) */
